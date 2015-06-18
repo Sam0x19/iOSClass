faedf5be4d5b80f6fda6b1462409cefd8d4eb2ae
Sam Scalise <sammyboy25@gmail.com>
2015-06-17 23:15:13 -0700

    initial commit

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Sam Scalise on 6/17/15.
+//  Copyright (c) 2015 Sam Scalise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+
+}
+
