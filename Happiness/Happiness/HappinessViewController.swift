808d725f83c233123d924e55e427eb7e2805999e
Sam Scalise <sammyboy25@gmail.com>
2015-06-22 22:36:53 -0700

    added face view code from lecture 5

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Sam Scalise on 6/22/15.
+//  Copyright (c) 2015 Sam Scalise. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+}
